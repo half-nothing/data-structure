778da19a70787774e29bd8a43a9c40338a58d177
Half_nothing <Half_nothing@163.com>
2023-06-03 00:56:52 +0800

    2023.06.03.4 修改文件编码

--- src/LinearList/test/DoubleLinkListTest.cpp
+++ src/LinearList/test/DoubleLinkListTest.cpp
@@ -3,32 +3,32 @@
 using namespace std;
 
 int main() {
-    // ³õÊ¼»¯Á´±í
+    // åˆå§‹åŒ–é“¾è¡¨
     DoubleLinkList<int> doubleLinkList{1, 2, 3, 4};
     cout << "At begin: " << endl << doubleLinkList;
-    // Ìí¼ÓÔªËØ
+    // æ·»åŠ å…ƒç´ 
     doubleLinkList.append({5, 6, 7});
     cout << "Add 5,6,7 to the end: " << endl << doubleLinkList;
-    // ²åÈëÔªËØ
+    // æ’å…¥å…ƒç´ 
     doubleLinkList.insert({{2, 8}, {2, 9}});
     cout << "Insert 8,9 to index 2: " << endl << doubleLinkList;
-    // É¾³ıÔªËØ
+    // åˆ é™¤å…ƒç´ 
     doubleLinkList.remove(2, 5);
     cout << "Remove elements index between 2 and 5: " << endl << doubleLinkList;
-    // »ñÈ¡ÔªËØÖµ
+    // è·å–å…ƒç´ å€¼
     cout << "Element at index 2 is " << doubleLinkList[2] << endl;
     cout << "Element at index 3 is " << doubleLinkList.find(3) << endl;
-    // ĞŞ¸ÄÔªËØÖµ
+    // ä¿®æ”¹å…ƒç´ å€¼
     doubleLinkList[2] = 10;
     cout << "Change element at index 2 to " << doubleLinkList[2] << endl;
     doubleLinkList.setValue(3, 12);
     cout << "Change element at index 3 to " << doubleLinkList[3] << endl;
     cout << doubleLinkList;
-    // ±éÀú²âÊÔ
+    // éå†æµ‹è¯•
     cout << "Every element add 1: " << endl;
     doubleLinkList.forEach(0, doubleLinkList.size(), [](int &tmp){ tmp += 1;});
     cout << doubleLinkList;
-    // ²éÕÒ²âÊÔ
+    // æŸ¥æ‰¾æµ‹è¯•
     cout << "Element 13 at index " << doubleLinkList.getPos(13) << endl;
     return 0;
 }
